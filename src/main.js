927d33f18d3dc76e518967ef57645215e2f4133a
“muzi2tap <250979762@qq.com>
2020-02-18 20:08:10 +0800

    登录完成

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+// 导入字体图标
+import "./assets/iconfont/iconfont.css"
+// 导入全局样式表
+import "./assets/global.css"
+
+import axios from "axios"
+// 配置请求的根路径
+axios.defaults.baseURL="http://127.0.0.1:8888/api/private/v1/"
+// axios请求拦截器,保证拥有获取数据的权限
+axios.interceptors.request.use(config=>{
+  config.headers.Authorization=window.sessionStorage.getItem("token");
+  return config;
+})
+Vue.prototype.$http=axios
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,53 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: "/home"
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/login',
+    name: 'Login',
+    component: () => import("../components/Login.vue")
+  },
+  {
+    path:"/home",
+    name:"home",
+    component:()=>import("../components/Home.vue"),
+    redirect: "/welcome",
+    children:[
+      {
+        path:"/welcome",
+        name:"welcome",
+        component:_=>import("../components/Welcome.vue")
+      },
+      {
+        path:"/users",
+        name:"users",
+        component:_=>import("../components/user/User.vue")
+      }
+    ]
+  },
+  
 ]
 
 const router = new VueRouter({
   routes
+})
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  if(to.path==="/login"){
+    return next();
+  }
+  const tokenStr=window.sessionStorage.getItem("token");
+  if(!tokenStr){
+    return next("/login")
+  }
+  next();
 })
 
 export default router
